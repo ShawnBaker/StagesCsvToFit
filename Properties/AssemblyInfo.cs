eadce13066dc825b915a8d30b74a8d51dc1fad0b
Shawn Baker <shawn@frozen.ca>
2019-10-01 17:44:27 -0500

    Version 1.0.2 to test updating.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StagesCsvToFit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Converts a CSV file from a Stages indoor bike into a FIT file.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Frozen North Computing")]
 [assembly: AssemblyProduct("StagesCsvToFit")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
